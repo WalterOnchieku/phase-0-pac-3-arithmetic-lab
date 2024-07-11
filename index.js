61a05e23bfad10238ad68da47f340f7f0792eeb0
root <root@DESKTOP-RHA5CJR.localdomain>
2024-07-11 01:33:39 -0700

    js calculator

---
+++ index.js
@@ -0,0 +1,40 @@
+
+function add(a, b) {
+    // we'll fill this in shortly
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    // we'll fill this in shortly
+    return a - b;
+  }
+
+  function multiply(a, b) {
+    // we'll fill this in shortly
+    return a * b;
+  }
+
+  function divide(a, b) {
+    // we'll fill this in shortly
+    return a / b;
+  }
+
+  function increment(n){
+   
+    return n+=1;
+  }
+
+  function decrement(n){
+
+    return n-=1;
+  }
+
+
+function makeInt(n){
+    let n = 48;
+    parseInt()
+}
+
+function preserveDecimal(n){
+    parseFloat()
+}
